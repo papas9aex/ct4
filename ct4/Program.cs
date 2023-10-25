94826c3ef7422cd5883af9d8d2a486287f311cd9
sss <glebtop4uk@mail.ru>
2023-10-25 17:01:34 +0300

    Добавьте файлы проекта.

---
+++ ct4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int result = DivideByTwo(num);
+            Console.WriteLine("Результат деления: " + result);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Ошибка: Введено некорректное число. " + ex.Message);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Ошибка: Деление на ноль. " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Произошла неожиданная ошибка. " + ex.Message);
+        }
+    }
+
+    static int DivideByTwo(int num)
+    {
+        if (num == 0)
+        {
+            throw new DivideByZeroException("Попытка деления на ноль.");
+        }
+        return num / 2;
+    }
+}
